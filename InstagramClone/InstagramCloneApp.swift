f647d8ec724ef2cab40a061f6d404adbe4fefa43
VickyIr <vicky.ir@students.amikom.ac.id>
2023-04-10 02:13:14 +0700

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Vicky Irwanto on 10/04/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
